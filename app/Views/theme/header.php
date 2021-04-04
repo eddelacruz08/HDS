b7ef68280ddaa1992384c3d208ce3f3097f370a5
shailynsaan <56763398+shailynsaan@users.noreply.github.com>
2021-04-04 14:24:45 +0800

    Update header.php

--- app/Views/theme/header.php
+++ app/Views/theme/header.php
@@ -5,72 +5,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="shortcut icon" href="<?= base_url() ?>/public/img/PUPLogo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= base_url() ?>/public/img/hds_logo_name.png" type="image/x-icon">
     <link rel="stylesheet" href="<?= base_url() ?>public/dist/css/adminlte.css">
-    <link href="<?= base_url() ?>public/plugins/select2/css/select2.min.css"/>
-    <link href="<?= base_url() ?>public/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css"/>
     <!-- <link rel="stylesheet" href="<?= base_url() ?>public/css/bootstrap-theme.min.css"> -->
     <link rel="stylesheet" href="<?= base_url() ?>public/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/font-awesome/css/all.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/css/select2.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/css/select2-bootstrap4.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/css/style.css">
+    <!-- <link href="<?= base_url() ?>public/select2/dist/css/select2.min.css" rel="stylesheet" /> -->
     <link href="<?= base_url() ?>public/plugins/chart.js/Chart.min.css" rel="stylesheet" />
     <link href="<?= base_url() ?>public/DataTables/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
     <link href="<?= base_url() ?>public/plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
     <script type="text/javascript" src="<?= base_url() ?>public/font-awesome/js/all.min.js"></script>
     <link rel="stylesheet" href="<?= base_url() ?>public/css/ionicons.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
     <script src="<?= base_url() ?>public/js/instascan.min.js"></script>
-    <script type="text/javascript" src="<?= base_url() ?>public/js/adapter.min.js"></script>
-    <script type="text/javascript" src="<?= base_url() ?>public/js/vue.min.js"></script>
-    <script type="text/javascript" src="<?= base_url() ?>public/js/dateScripts.js"></script>
-    <link href="<?= base_url() ?>public/css/print.css" rel="stylesheet" media="print"/>
-
     <style media="screen">
-    .buttun {
-          background-color: #004A7F;
-          -webkit-border-radius: 10px;
-          border-radius: 10px;
-          border: none;
-          color: #FFFFFF;
-          cursor: pointer;
-          display: inline-block;
-          font-family: Arial;
-          font-size: 20px;
-          padding: 5px 10px;
-          text-align: center;
-          text-decoration: none;
-          -webkit-animation: glowing 1500ms infinite;
-          -moz-animation: glowing 1500ms infinite;
-          -o-animation: glowing 1500ms infinite;
-          animation: glowing 1500ms infinite;
-          }
-          @-webkit-keyframes glowing {
-          0% { background-color: #B20000; -webkit-box-shadow: 0 0 3px #B20000; }
-          50% { background-color: #FF0000; -webkit-box-shadow: 0 0 40px #FF0000; }
-          100% { background-color: #B20000; -webkit-box-shadow: 0 0 3px #B20000; }
-          }
-
-          @-moz-keyframes glowing {
-          0% { background-color: #B20000; -moz-box-shadow: 0 0 3px #B20000; }
-          50% { background-color: #FF0000; -moz-box-shadow: 0 0 40px #FF0000; }
-          100% { background-color: #B20000; -moz-box-shadow: 0 0 3px #B20000; }
-          }
-
-          @-o-keyframes glowing {
-          0% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-          50% { background-color: #FF0000; box-shadow: 0 0 40px #FF0000; }
-          100% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-          }
-
-          @keyframes glowing {
-          0% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-          50% { background-color: #FF0000; box-shadow: 0 0 40px #FF0000; }
-          100% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-          }
-    </style>
-    <style media="screen">
-
     .box{
           display: none;
           margin-left: 25px;
@@ -102,11 +54,11 @@
     </style>
     <title><?= SYSTEM_NAME ?></title>
   </head>
-  <body class="hold-transition sidebar-mini layout-fixed hidePrint"style="background-color: #d9d9d9;"  onload="renderTime();">
-  <div class="wrapper"style="background-color: #d9d9d9;">
+  <body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper" style="background-color: #d9d9d9;">
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light  elevation-8">
       <!-- Left navbar links -->
       <ul class="navbar-nav">
         <li class="nav-item">
@@ -139,10 +91,10 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary bg-navy" >
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 bg-navy" >
       <!-- Brand Logo -->
       <a href="<?= base_url() ?>patients" class="brand-link">
-        <img src="http://localhost/HDS/public/img/PUPLogo.png" alt="PUP Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="http://localhost/HDS/public/img/hds_logo_name_white.png" alt="hds logo name" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><?= SYSTEM_TITLE ?></span>
       </a>
 
@@ -194,5 +146,4 @@
 
 
       <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper"style="background-color: #d9d9d9;">
-        <div class="container-fluid" style="background-color: #d9d9d9;">
+      <div class="content-wrapper" style="background-color: #d9d9d9;">
